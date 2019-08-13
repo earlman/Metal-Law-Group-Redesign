48e92ccda865bfacae454cfc4b22585047c2a790
earlman <earl.mangulabnan@gmail.com>
2019-08-13 17:34:10 -0500

    Create Card Grid Component

---
+++ src/components/CardGrid.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="cardgrid">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+
+.cardgrid
+    display: grid
+    grid-template-columns: 1fr
+    grid-template-rows: auto
+    justify-items: stretch
+    
+    @include md
+        grid-template-columns: 1fr 1fr
+
+
+</style>
