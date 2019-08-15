495ec6152160260523b6150bb28395de1b190cb7
earlman <earl.mangulabnan@gmail.com>
2019-08-15 10:31:15 -0500

    fixed footer layout on all devices

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,26 +1,26 @@
 <template>
-	<footer>
-		<nav>
+	<footer class="footer">
+		<nav class="footer--nav">
 			<div class="footer--logo">
 				<img src alt class="logo" />
 				(LOGO)
 			</div>
 			<div class="footer--links p-small">
-				<div>
+				<div class="footer--submenu">
 					<h5>Our Services</h5>
 					<ul>
 						<li>Business Litigation</li>
 						<li>Employment Law Defense</li>
 					</ul>
 				</div>
-				<div>
+				<div class="footer--submenu">
 					<h5>Quick Links</h5>
 					<ul>
 						<li>Disclaimer</li>
 						<li>Privacy Policy</li>
 					</ul>
 				</div>
-				<div>
+				<div class="footer--submenu">
 					<h5>Contact</h5>
 					<ul>
 						<li>Phone: 323-289-2260</li>
@@ -31,7 +31,7 @@
 						</li>
 					</ul>
 				</div>
-				<div>
+				<div class="footer--social">
 					<ul>
 						<li>Fb</li>
 						<li>Tw</li>
@@ -52,32 +52,63 @@ export default {};
 
 <style lang="sass" scoped>
 
-    footer
+    .footer
         // border: 1px solid red //* for development, delete later
         justify-self: center
         background-color: var(--color-b-alt)
         width: 100vw
-
-        .footer--logo
-            grid-column: 1/-1
-
-        .footer--links
-            grid-column: 1/-1
-            display: flex
-
-            &>*
-                @include space-inline(var(--space-lg))
-
-        nav
-            @include grid-main
-            justify-items: center
-
-            h5
-                color: var(--color-t-h-alt)
+        display: flex
+        flex-direction: column
+        align-items: center
+        
+        &>*
+            max-width: 1400px
 
         *
             color: white
 
+        &--nav
+            display: flex
+            width: 100%
+            justify-content: space-around
+            justify-items: center
+            flex-wrap: wrap
+
+
+            h5
+                color: var(--color-t-h-alt)
+
+
+        &--logo
+            flex-grow: 1
+            text-align: center
+            min-width: 200px
+
+        &--links
+            display: flex
+            flex-grow: 4
+            justify-content: center
+            flex-wrap: wrap
+            max-width: 90vw
+
+            @include landscape
+                justify-content: space-between
+
+            &>*
+                min-width: 200px
+                flex-grow: 1
+
+            ul
+                padding: 0
+
+        &--social ul
+            display: flex
+            min-width: 10ch
+            flex-grow: 2
+            justify-content: center
+
+            li
+                @include space-inline(var(--space-xs))
 
         .copyright
             @include grid-full
