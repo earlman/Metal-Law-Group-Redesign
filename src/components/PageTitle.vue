cde50a0c431a3b0ddafbda768dc0f32e8a836158
earlman <earl.mangulabnan@gmail.com>
2019-08-20 18:06:54 -0500

    Create & Implement Page TItle Component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="page-title">
+		<h1>
+			<slot>Insert Title into Component via Slot</slot>
+		</h1>
+		<slot
+			v-if="this.$slots.detail"
+			name="detail"
+			class="section-title--detail"
+		>Additional content goes into `detail` slot</slot>
+
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+
+    .page-title
+        display: flex
+        flex-direction: column
+        align-items: center
+        text-align: center
+
+        h1
+            margin: 0 0 var(--space-md)
+
+    
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,70 +1,76 @@
 <template>
 	<Layout>
-		<section>
-			<div class="panelcomponent page-title">
-				<h1>We are a full business law firm with a solid reputation for providing effective and zealous representation.</h1>
-				<button>Contact us for a free consultation&nbsp;&nbsp;&#8594;</button>
-			</div>
-		</section>
-		<SectionTitle>Our Services</SectionTitle>
-		<section>
-			<CardGrid>
-				<Card>
-					Business Litigation Card
+		<main>
+			<section>
+				<PageTitle>
+					We are a full business law firm with a solid reputation for providing effective and zealous representation.
 					<template
 						v-slot:detail
-					>We can help you in breach of contract cases, joint partnership ventures gone bad, LLC lawsuits, and other business disputes.</template>
-				</Card>
-				<Card>
-					Employment Defense
-					<template
-						v-slot:detail
-					>We will help you build a strong line of defense to protect your business from the unfair state laws and vindictive former employees.</template>
-				</Card>
-			</CardGrid>
-		</section>
-		<SectionTitle>Get in Touch</SectionTitle>
-		<section class="get-in-touch">
-			<div class="info">
-				<h5>Phone:</h5>
-				<h5>323-289-2260</h5>
-				<h5>Fax:</h5>
-				<h5>323-289-2261</h5>
-				<h5>Address:</h5>
-				<h5>
-					725 S Figueroa Street #3065
-					<br />Los Angeles, CA 90017
-				</h5>
-			</div>
-			<div class="form">
-				<form action>
-					Name
-					<input type="text" />
-					Phone
-					<input type="tel" name id />
-					Email
-					<input type="email" name id />
-					Message
-					<textarea></textarea>
-				</form>
-			</div>
-		</section>
-		<SectionTitle>
-			Find a Professional
-			<template v-slot:detail>
-				<input type="search" name id />
-			</template>
-		</SectionTitle>
-		<SectionTitle>Our Success Stories</SectionTitle>
-		<section>
-			<div class="testimonialcomponent">
-				<div>
-					<p>“Richard Jefferson is a Mensch and a gentleman who is also well-versed in cutting-edge legal developments that he employs to help his clients, from the large to the very small. He is especially good about looking after the business interests of the ‘little guy’ and is just as well equipped to handle large, complex and far reaching deals.”</p>
-					<h4 class="testimonial--name">Tess Taylor</h4>
-					<h5 class="testimonial--job">President Narip</h5>
+					>
+						<button>Contact us for a free consultation&nbsp;&nbsp;&#8594;</button>
+					</template>
+				</PageTitle>
+			</section>
+			<SectionTitle>Our Services</SectionTitle>
+			<section>
+				<CardGrid>
+					<Card>
+						Business Litigation Card
+						<template
+							v-slot:detail
+						>We can help you in breach of contract cases, joint partnership ventures gone bad, LLC lawsuits, and other business disputes.</template>
+					</Card>
+					<Card>
+						Employment Defense
+						<template
+							v-slot:detail
+						>We will help you build a strong line of defense to protect your business from the unfair state laws and vindictive former employees.</template>
+					</Card>
+				</CardGrid>
+			</section>
+			<SectionTitle>Get in Touch</SectionTitle>
+			<section class="get-in-touch">
+				<div class="info">
+					<h5>Phone:</h5>
+					<h5>323-289-2260</h5>
+					<h5>Fax:</h5>
+					<h5>323-289-2261</h5>
+					<h5>Address:</h5>
+					<h5>
+						725 S Figueroa Street #3065
+						<br />Los Angeles, CA 90017
+					</h5>
 				</div>
-			</div>
-		</section>
+				<div class="form">
+					<form action>
+						Name
+						<input type="text" />
+						Phone
+						<input type="tel" name id />
+						Email
+						<input type="email" name id />
+						Message
+						<textarea></textarea>
+					</form>
+				</div>
+			</section>
+			<SectionTitle>
+				Find a Professional
+				<template v-slot:detail>
+					<input type="search" name id />
+				</template>
+			</SectionTitle>
+			<SectionTitle>Our Success Stories</SectionTitle>
+			<section>
+				<div class="testimonialcomponent">
+					<div>
+						<p>“Richard Jefferson is a Mensch and a gentleman who is also well-versed in cutting-edge legal developments that he employs to help his clients, from the large to the very small. He is especially good about looking after the business interests of the ‘little guy’ and is just as well equipped to handle large, complex and far reaching deals.”</p>
+						<h4 class="testimonial--name">Tess Taylor</h4>
+						<h5 class="testimonial--job">President Narip</h5>
+					</div>
+				</div>
+			</section>
+		</main>
 	</Layout>
 </template>
 
@@ -93,10 +99,12 @@ import Post from "~/components/Post.vue";
 import SectionTitle from "~/components/SectionTitle.vue";
 import Card from "~/components/Card.vue";
 import CardGrid from "~/components/CardGrid.vue";
+import PageTitle from "~/components/PageTitle.vue";
 
 export default {
 	components: {
 		Pager,
+		PageTitle,
 		Post,
 		SectionTitle,
 		Card,
@@ -113,14 +121,6 @@ export default {
     section
         padding: var(--space-lg) 0
 
-    .page-title
-        display: flex
-        flex-direction: column
-        align-items: center
-        text-align: center
-
-        h1
-            margin: 0 0 var(--space-md)
 
     .testimonialcomponent
         display: flex
