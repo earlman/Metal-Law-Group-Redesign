02bab6bc3e425119a1380282d805b72633b019d3
earlman <earl.mangulabnan@gmail.com>
2019-08-20 17:54:07 -0500

    Create EmploymentLawDefense page

---
+++ src/pages/EmploymentLawDefense.vue
@@ -0,0 +1,16 @@
+<template>
+	<Layout></Layout>
+</template>
+
+<script>
+import SectionTitle from "~/components/SectionTitle.vue";
+import Card from "~/components/Card.vue";
+import CardGrid from "~/components/CardGrid.vue";
+
+export default {};
+</script>
+
+<style lang="sass" scoped>
+    
+
+</style>
