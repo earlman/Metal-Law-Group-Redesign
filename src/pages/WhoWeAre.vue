ff300c0ca6bbda3bb0294bb735818ca1bd847ac0
earlman <earl.mangulabnan@gmail.com>
2019-08-20 23:48:58 -0500

    create who we are page

---
+++ src/pages/WhoWeAre.vue
@@ -0,0 +1,36 @@
+<template>
+	<Layout>
+		<PageTitle>
+			Who We Are
+			<template v-slot:detail>
+				Metal Law Group exists to advise, defend and protect business owners, entrepreneurs and employers.
+				<input
+					type="search"
+					name
+					id
+				/>
+			</template>
+		</PageTitle>
+	</Layout>
+</template>
+
+<script>
+import SectionTitle from "~/components/SectionTitle.vue";
+import PageTitle from "~/components/PageTitle.vue";
+import Card from "~/components/Card.vue";
+import CardGrid from "~/components/CardGrid.vue";
+
+export default {
+	components: {
+		SectionTitle,
+		PageTitle,
+		Card,
+		CardGrid
+	}
+};
+</script>
+
+<style lang="sass" scoped>
+    
+
+</style>
