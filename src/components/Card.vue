93cc6bc299a1c9d2c187ee4b59e64804a2a5e418
earlman <earl.mangulabnan@gmail.com>
2019-08-26 10:45:04 -0500

    fixed card on all devices

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,7 +12,9 @@
 					>We can help you in breach of contract cases, joint partnership ventures gone bad, LLC lawsuits, and other business disputes.</slot>
 				</p>
 			</div>
-			<img src class="card--arrow" alt="arrow" />
+			<div class="card--arrow">
+				<img src alt="arrow" />
+			</div>
 		</div>
 	</div>
 </template>
@@ -43,51 +45,39 @@ export default {
 
 .card
     display: flex
-    padding: var(--space-lg) var(--space-sm)
-    
+    padding: var(--space-lg) var(--space-sm) var(--space-md)
     flex-direction: column
+    height: 100%
 
     &--title
         margin-bottom: var(--space-sm)
 
-    &--info
-        // flex-grow: 1
-
-    // when there's 2 columns, change the layout. See CardGrid.vue
-    @include md 
-        flex-direction: row
-    
-        &--icon
-            padding: 0 var(--space-sm)
-
-
     &--detail
         max-width: 400px
 
+    &--info
+        margin-bottom: var(--space-sm)
+
     &--arrow
-        position: relative
-        bottom: -2rem
         align-self: end
-        
+        // position: relative
+        // bottom: -2rem
+        // padding: var(--space-xs)        
+
+    &--icon
+        margin-bottom: var(--space-xs)
+
+    // when there's 2 columns, change the layout. See CardGrid.vue
+    @include lg
+        flex-direction: row
+        margin: 0 var(--space-lg)
+
+        &--icon
+            margin-right: var(--space-md)    
 
 
 
 
 
 
-
-// .section--title
-//     left: 50%
-//     margin-left: -51vw
-//     margin-right: -51vw
-//     position: relative
-//     right: 50%
-//     border: 1px solid var(--color-a)
-
-//     h2
-//         box-sizing: border-box
-//         padding: var(--space-sm)
-//         text-align: center
-//         width: 102vw
-//         margin: 0
 </style>
