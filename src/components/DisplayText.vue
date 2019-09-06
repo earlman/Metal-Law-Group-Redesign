748b41ffa09e7deb42e4767ab923f2b90fc66ce5
earlman <earl.mangulabnan@gmail.com>
2019-09-06 15:09:23 -0500

    build DisplayText Component

--- src/components/DisplayText.vue
+++ src/components/DisplayText.vue
@@ -11,19 +11,33 @@
 
 
 <template>
-    <div>
-
-        
-
-    </div>
+	<div>
+		<div class="display-text--container">
+			<div class="display-text--content">
+				<slot></slot>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="sass" scoped>
+
+.display-text
+
+    &--content
+        margin: auto
+
+
+    &--container
+        margin: var(--space-lg) auto
+        padding: var(--space-md)
+        background-color: var(--color-a)
+        max-width: 768px
+        display: flex
+        justify-content: center
 
 </style>
