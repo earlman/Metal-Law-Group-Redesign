104c6f116dc14713abaff7bdfec404d7c87084a0
earlman <earl.mangulabnan@gmail.com>
2019-09-09 18:57:38 -0500

    fix margins on PageTitle

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,10 +1,12 @@
 <template>
 	<div class="page-title">
-		<h1>
-			<slot>Insert Title into Component via Slot</slot>
-		</h1>
-		<div class="page-title--detail">
-			<slot v-if="this.$slots.detail" name="detail">Additional content goes into `detail` slot</slot>
+		<div class="page-title--inner">
+			<h1>
+				<slot>Insert Title into Component via Slot</slot>
+			</h1>
+			<div class="page-title--detail">
+				<slot v-if="this.$slots.detail" name="detail">Additional content goes into `detail` slot</slot>
+			</div>
 		</div>
 	</div>
 </template>
@@ -32,10 +34,17 @@ export default {};
         right: 50%
         width: 102vw
 
+
+        &--inner
+            max-width: 88vw
+
+
         h1
             margin: 0 0 var(--space-sm)
+            // max-width: 88vw
 
         &--detail p
             max-width: 75ch
+            
     
 </style>
