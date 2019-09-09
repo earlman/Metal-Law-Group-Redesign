683b8e736a3a863d5a97f55c924a094ac953b3dc
earlman <earl.mangulabnan@gmail.com>
2019-09-09 14:03:44 -0500

    add indicator

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -9,13 +9,23 @@
 
 <template>
 	<div>
-		<div v-for="testimonial in testimonials" :key="testimonial.name">
-			<div class="testimonials--container" v-if="testimonial.active">
-				<div class="testimonials">
-					<p class="testimonials--content">{{testimonial.content}}</p>
-					<h4 class="testimonials--name">{{testimonial.name}}</h4>
-					<h5 class="testimonials--occupation">{{testimonial.occupation}}</h5>
+		<div class="testimonials">
+			<div v-for="testimonial in testimonials" :key="testimonial.name">
+				<div class="testimonials--container" v-if="testimonial.active">
+					<div class="testimonials--content">
+						<p class="testimonials--quote">{{testimonial.quote}}</p>
+						<h4 class="testimonials--name">{{testimonial.name}}</h4>
+						<h5 class="testimonials--occupation">{{testimonial.occupation}}</h5>
+					</div>
 				</div>
+			</div>
+			<div class="testimonials--selector">
+				<div
+					v-for="testimonial in testimonials"
+					:key="testimonial.id"
+					class="indicator"
+					:class="{ 'active': testimonial.active }"
+				/>
 			</div>
 		</div>
 	</div>
@@ -27,21 +37,24 @@ export default {
 		return {
 			testimonials: [
 				{
-					content:
+					id: 1,
+					quote:
 						"Richard Jefferson is a Mensch and a gentleman who is also well-versed in cutting-edge legal developments that he employs to help his clients, from the large to the very small. He is especially good about looking after the business interests of the ‘little guy’ and is just as well equipped to handle large, complex and far reaching deals.",
 					name: "Tess Taylor",
 					occupation: "President Narip",
 					active: false
 				},
 				{
-					content:
+					id: 2,
+					quote:
 						"Richard Jefferson is a Mensch and a gentleman who is also well-versed in cutting-edge legal developments that he employs to help his clients, from the large to the very small. He is especially good about looking after the business interests of the ‘little guy’ and is just as well equipped to handle large, complex and far reaching deals.",
 					name: "David Rosenhaus",
 					occupation: "Owner of DFN Graphics Company",
 					active: true
 				},
 				{
-					content:
+					id: 3,
+					quote:
 						"Richard Jefferson is a Mensch and a gentleman who is also well-versed in cutting-edge legal developments that he employs to help his clients, from the large to the very small. He is especially good about looking after the business interests of the ‘little guy’ and is just as well equipped to handle large, complex and far reaching deals.",
 					name: "Jasmine Diaz",
 					occupation: "Owner of The J. Diaz Company",
@@ -56,22 +69,39 @@ export default {
 <style lang="sass" scoped>
 
 .testimonials
-    display: flex
-    flex-direction: column
-    align-items: center
-    text-align: center
 
     &--content
+        display: flex
+        flex-direction: column
+        align-items: center
+        text-align: center
+
+    &--quote
         margin-bottom: var(--space-md)
 
     &--container
-        margin: var(--space-lg) auto
+        margin: var(--space-lg) auto var(--space-sm)
         padding: var(--space-md) 0 var(--space-sm)
         background-color: var(--color-a)
         max-width: 768px
         display: flex
         justify-content: center
 
-    &--indicator
+    &--selector
+        display: flex
+        flex-direction: row
+        justify-content: center
+        margin-bottom: var(--space-lg)
+
+
+
+        .indicator
+            @include space-inline(var(--space-xs))
+            height: 5px
+            width: 48px
+            background-color: var(--color-a)
+
+        .active
+            background-color: var(--color-p)
 
 </style>
