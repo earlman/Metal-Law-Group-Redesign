b52c16e5fae8f6dcfe4bd1c4e494e93cfbed7c5d
earlman <earl.mangulabnan@gmail.com>
2019-10-31 15:42:11 -0500

    create modified versions of Card and CardGrid

---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="card-wrapper">
+		<div class="card">
+			<Icon :name="iconname" size="32pt" class="card--icon" color="var(--color-p)" />
+
+			<div class="card--info">
+				<h3 class="card--title">
+					<slot>Business Litigation Card</slot>
+				</h3>
+				<p class="card--detail">
+					<slot
+						name="detail"
+					>We can help you in breach of contract cases, joint partnership ventures gone bad, LLC lawsuits, and other business disputes.</slot>
+				</p>
+			</div>
+			<div class="card--arrow">
+				<g-link :to="linkto">
+					<Icon :name="icons.continue" size="20pt" />
+				</g-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Icon from "~/components/Icon.vue";
+import icons from "~/icons.js";
+
+export default {
+	components: {
+		Icon
+	},
+	props: {
+		iconname: {
+			type: Object,
+			default: icons.search
+		},
+		linkto: {
+			type: String,
+			default: "/contact-us"
+		}
+	},
+	data: () => ({
+		icons
+	})
+};
+</script>
+
+<style lang="sass" scoped>
+
+
+.card-wrapper
+
+    width: 100%
+    padding: var(--space-lg) var(--space-sm) var(--space-md)
+
+    // &:nth-child(odd) .card
+    //     justify-content: flex-end
+    // &:nth-child(even) .card
+    //     justify-content: flex-start
+
+
+
+.card
+    display: flex
+    flex-direction: column
+    height: 100%
+    justify-content: center
+
+    &--title
+        margin-bottom: var(--space-sm)
+
+    &--detail
+        max-width: 400px
+
+    &--info
+        margin-bottom: var(--space-sm)
+
+    &--arrow
+        align-self: flex-end
+        margin-left: var(--space-sm)
+        // position: relative
+        // bottom: -2rem
+        // padding: var(--space-xs)        
+
+    &--icon
+        margin-bottom: var(--space-xs)
+        width: 40px
+
+
+    // when there's 2 columns, change the layout. See CardGrid.vue
+    @include lg
+        flex-direction: row
+        margin: 0 var(--space-md)
+
+        &--icon
+            margin-right: var(--space-md)    
+
+
+
+
+
+
+</style>
---
+++ src/components/EmployeeCardGrid.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="cardgrid">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+
+.cardgrid
+    display: grid
+    grid-template-columns: 1fr
+    grid-template-rows: auto
+    justify-items: stretch
+
+    border: 1px solid var(--color-a)
+
+    
+    @include md
+        grid-template-columns: 1fr 44vw 44vw 1fr
+        &>*
+            &:nth-child(odd) 
+                padding-left: 0
+            &:nth-child(even)
+                padding-right: 0
+        
+
+        @include xl
+            grid-template-columns: 1fr 700px 700px 1fr
+
+    & > *
+        &:nth-child(odd) 
+            border-left: none
+            border-right: 1px solid var(--color-a)
+
+        &:nth-child(even)
+            border-left: 1px solid var(--color-a)
+            border-right: none
+
+    &::before,
+    &::after
+        content: ''
+
+
+</style>
