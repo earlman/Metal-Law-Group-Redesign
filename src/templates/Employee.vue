bd3ee357f3ae4398415b357f2b0ba877685a74b7
earlman <earl.mangulabnan@gmail.com>
2019-10-31 07:47:55 -0500

    update employee template to use queried data

--- src/templates/Employee.vue
+++ src/templates/Employee.vue
@@ -1,15 +1,16 @@
 <template>
 	<Layout>
-		<h1>Does it work????? {{$page.employee.title}}</h1>
 		<section class="general">
 			<div class="section--inner">
 				<div class="general--photo">
-					<img src="https://via.placeholder.com/400.png" alt="Employee Photo" />
+					<g-image :src="$page.employee.acf.photo" alt="Employee Photo" width="500" />
+
+					<!-- <img :src="$page.employee.acf.photo" alt="Employee Photo" /> -->
 				</div>
 				<div class="general--info">
-					<h1 class="general--name">Rosie Palmer</h1>
-					<h2>Managing Partner</h2>
-					<div class="general--details">
+					<h1 class="general--name">{{$page.employee.acf.name}}</h1>
+					<h2>{{$page.employee.acf.title}}</h2>
+					<!-- <div class="general--details">
 						<div>
 							<h5>Phone</h5>
 							<p>(630) 235 1515</p>
@@ -18,7 +19,7 @@
 							<h5>Email</h5>
 							<p>palmar@metallawgroup.com</p>
 						</div>
-					</div>
+					</div>-->
 				</div>
 			</div>
 		</section>
@@ -26,39 +27,22 @@
 			<section class="experience">
 				<div class="section--inner">
 					<h2>Experience</h2>
-					<p>
-						Dan is currently the Litigation Managing Partner and lead trial attorney at M.E.T.A.L. Law Group, LLP.
-						His practice is focused on labor & employment, commercial & business litigation, corporate law, franchise law, church-related litigation, and representing news media and publisher in defamation cases.
-						He was a law clerk and a staff attorney in the Law Department, Commercial Calendar, of the Circuit Court of Cook County, Illinois.
-						He has practiced in Illinois, the District of Columbia and California.
-						He is a member of the Federal Trial Bar for the Northern District of Illinois. Also, he has been a certified mediator since 2001 in Chicago.
-						Dan has represented both employers and employees in labor disputes (but now his focus is only on employers) regarding wage claims, wrongful termination, and violations of state and federal labor law.
-						He has tried complex commercial and business matters, and recently, obtained a multi-million-dollar settlement based on cross-complaint on the eve of a three-week jury trial in Chicago.
-						Also, he recently obtained a non-suit against plaintiffs seeking 20 million dollars in a defamation action against a newspaper and online publisher in the District of Columbia.
-						Dan is known as one of the most prominent trial attorneys for the church-related matters concerning property and control disputes, clergy malpractice and defamation.
-						He has taught church polity of various denominations at seminaries and published and lectured on church-related issues. He has tried over 70 cases representing in both civil and ecclesiastical courts.
-						Most recently, he successfully represented Holy Hill Community Church in the Superior Court of the State of California, which was affirmed on appeal, in an action entitled Jun Ki Kim, et al. v. The True Church Member of the Holy Hill Community Church, et al. (2014) 236 Cal. App. 4th 1435. This case is considered one of the most complex church litigation involving civil and ecclesiastical laws concerning property.
-						As part of community service, Dan often provides pro bono legal services to indigenous and minority groups, especially for victims of domestic violence whose resources are limited due to their legal status, language and/or financial means.
-						In addition to his legal profession, Dan was assistant professor of philosophy and theology at Northern Seminary (Lombard, IL). He brings his unique skill of teaching for trial.
-					</p>
+					<p>{{$page.employee.acf.experience}}</p>
 				</div>
 			</section>
 			<section class="additional-info">
 				<div class="section--inner">
 					<h5>Bar Admissions</h5>
-					<ul>California</ul>
+					<ul>
+						<li>{{$page.employee.acf.barAdmissions}}</li>
+					</ul>
 					<h5>Practice Areas</h5>
 					<ul>
-						<li>Litigation and Trials</li>
-						<li>Commercial and Business Litigation</li>
-						<li>Corporate Law</li>
-						<li>Church-Related Litigation</li>
-						<li>New Media Defamination</li>
+						<li>{{$page.employee.acf.practiceAreas}}</li>
 					</ul>
 					<h5>Education</h5>
 					<ul>
-						<li>BA * MA in philosophy, Baylor University</li>
-						<li>Master of Divinity</li>
+						<li>{{$page.employee.acf.education}}</li>
 					</ul>
 				</div>
 			</section>
@@ -105,6 +89,8 @@ export default {};
         width: 88vw
         max-width: 1200px
         margin: auto
+        white-space: pre-line //magically converts '/n' to new lines
+
 
     section
         
@@ -165,13 +151,13 @@ export default {};
         padding-top: var(--space-md)
         margin-bottom: var(--space-lg)
 
-
         ul
             padding-left: 20px
             list-style: none
 
             li
                 line-height: 2.5
+            
         
     .bottom-container
         @include lg
@@ -186,8 +172,6 @@ export default {};
 
             .additional-info
                 margin-top: var(--space-lg)
-
-
 
 
 </style>
