e280b8feb6e056f7f1094d348dd72178ff7939ac
earlman <earl.mangulabnan@gmail.com>
2019-11-11 19:19:06 -0600

    refactor layout

--- src/templates/Employee.vue
+++ src/templates/Employee.vue
@@ -1,16 +1,15 @@
 <template>
-	<Layout>
+	<Layout class="employee">
 		<section class="general">
-			<div class="section--inner">
-				<div class="general--photo">
-					<g-image :src="$page.employee.acf.photo" alt="Employee Photo" width="500" />
+			<div class="general--photo">
+				<g-image :src="$page.employee.acf.photo" alt="Employee Photo" width="500" />
 
-					<!-- <img :src="$page.employee.acf.photo" alt="Employee Photo" /> -->
-				</div>
-				<div class="general--info">
-					<h1 class="general--name">{{$page.employee.acf.name}}</h1>
-					<h2>{{$page.employee.acf.title}}</h2>
-					<!-- <div class="general--details">
+				<!-- <img :src="$page.employee.acf.photo" alt="Employee Photo" /> -->
+			</div>
+			<div class="general--info">
+				<h1 class="general--name">{{$page.employee.acf.name}}</h1>
+				<h2>{{$page.employee.acf.title}}</h2>
+				<!-- <div class="general--details">
 						<div>
 							<h5>Phone</h5>
 							<p>(630) 235 1515</p>
@@ -19,32 +18,27 @@
 							<h5>Email</h5>
 							<p>palmar@metallawgroup.com</p>
 						</div>
-					</div>-->
-				</div>
+				</div>-->
 			</div>
 		</section>
 		<div class="bottom-container">
 			<section class="experience">
-				<div class="section--inner">
-					<h2>Experience</h2>
-					<p>{{$page.employee.acf.experience}}</p>
-				</div>
+				<h2>Experience</h2>
+				<p>{{$page.employee.acf.experience}}</p>
 			</section>
 			<section class="additional-info">
-				<div class="section--inner">
-					<h5>Bar Admissions</h5>
-					<ul>
-						<li>{{$page.employee.acf.barAdmissions}}</li>
-					</ul>
-					<h5>Practice Areas</h5>
-					<ul>
-						<li>{{$page.employee.acf.practiceAreas}}</li>
-					</ul>
-					<h5>Education</h5>
-					<ul>
-						<li>{{$page.employee.acf.education}}</li>
-					</ul>
-				</div>
+				<h5>Bar Admissions</h5>
+				<ul>
+					<li>{{$page.employee.acf.barAdmissions}}</li>
+				</ul>
+				<h5>Practice Areas</h5>
+				<ul>
+					<li>{{$page.employee.acf.practiceAreas}}</li>
+				</ul>
+				<h5>Education</h5>
+				<ul>
+					<li>{{$page.employee.acf.education}}</li>
+				</ul>
 			</section>
 		</div>
 	</Layout>
@@ -80,56 +74,42 @@ export default {};
 
 <style lang="sass" scoped>
 
-    /* This code is very low quality right now. */
-
-    /* We're gonna need system-wide changes for consistent grids accross the site. */
-
-    
-    .section--inner
-        width: 88vw
-        max-width: 1200px
-        margin: auto
+    .employee
         white-space: pre-line //magically converts '/n' to new lines
+    
+    .general
+        padding-top: var(--space-lg)
+        padding-bottom: var(--space-md)
+        border-bottom: 2px solid var(--color-a)
+        @include grid-main
+        max-width: 1200px
 
-
-    section
-        
-        padding: 0 var(--space-sm)
+        &>*
+            @include grid-full
 
         @include md
-            padding: 0 var(--space-md)
 
-
-    .general
-        padding-top: var(--space-md)
-        padding-bottom: var(--space-sm)
-        border-bottom: 1px solid var(--color-a)
-
-
-        @include lg
-            padding-top: var(--space-lg)
-            padding-bottom: var(--space-lg)
-
-            .section--inner
-                display: grid
-                grid-template-columns: min-content auto
+            .general--photo
+                grid-column: 1 / 3
 
             .general--info
+                grid-column: 3 / 7
                 margin-left: var(--space-lg)
 
-            .general--name
-                margin-top: 0
+            @include lg
+                padding-left: var(--space-sm)
+                padding-right: var(--space-sm)
+
+                .general--photo
+                    grid-column: 1 / 4
+
+                .general--info
+                    grid-column: 4 / 13
 
         &--photo
             margin: auto
             width: 100%
-
-            @include md
-                max-width: 300px
-                width: 30vw
-
-                @include lg
-                    margin-left: var(--space-md)
+            max-width: 400px
 
         &--name
             margin-bottom: 0
@@ -144,34 +124,33 @@ export default {};
                 flex-grow: 1
                 min-width: 10ch
 
-    .experience
+    .bottom-container
         padding-top: var(--space-md)
+        padding-bottom: var(--space-md)
+
+        @include grid-main
+        max-width: 1200px
+
+
+        &>*
+            @include grid-full
+
+        @include lg
+            padding-left: var(--space-sm)
+            padding-right: var(--space-sm)
+            .experience
+                grid-column: 1 / 10
+
+            .additional-info
+                grid-column: 10 / 13
 
     .additional-info
-        padding-top: var(--space-md)
-        margin-bottom: var(--space-lg)
-
         ul
             padding-left: 20px
             list-style: none
 
             li
                 line-height: 2.5
+                
             
-        
-    .bottom-container
-        @include lg
-            max-width: 1200px
-            margin: auto
-            margin-bottom: var(--space-lg)
-            display: grid
-            grid-template-columns: 2fr 1fr
-
-            .section--inner
-                display: contents
-
-            .additional-info
-                margin-top: var(--space-lg)
-
-
 </style>
