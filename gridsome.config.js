f7f4b04a66302a9714b8a0ed18ab7bf35e232c4a
earlman <earl.mangulabnan@gmail.com>
2020-03-03 14:05:21 -0600

    remove WordpressPage from gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -53,15 +53,15 @@ module.exports = {
     templates: {
         Employee: '/employee/:acf__name',
 
-        WordPressPage: [
-            {
-                path: '/employee/:slug',
-                component: './src/templates/Employee.vue',
-            }
-        ],
-        WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
-        WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
-        WordPressPostTag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+        // WordPressPage: [
+        //     {
+        //         path: '/employee/:slug',
+        //         component: './src/templates/Employee.vue',
+        //     }
+        // ],
+        // WordPressCategory: '/category/:slug', // adds route for "category" post type (Optional)
+        // WordPressPost: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
+        // WordPressPostTag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
 
     }
 }
